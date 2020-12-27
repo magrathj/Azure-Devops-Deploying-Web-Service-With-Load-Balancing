38255d09bc58d47e326c80e0bce7ac3111385add
jared.magrath <jared.magrath@newsignature.com>
2020-12-27 13:52:29 +0000

    adding template files for project

--- deploy_azure_policies.sh
+++ deploy_azure_policies.sh
@@ -1,3 +1,5 @@
+
+
 # Create the Policy Definition (Subscription scope)
 az policy definition create --name audit-existing-linux-vm-ssh-with-password --display-name "Audit existing Linux VMs that use password for SSH authentication" --description "This policy audits if a password is being used to authentication to a Linux VM" --rules ./azure_policies/azurepolicies.rules.json --mode All
 
