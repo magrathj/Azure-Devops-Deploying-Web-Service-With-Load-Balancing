3b084d1706a9aa974e7ddfb3400701aec6c6e0a9
jared.magrath <jared.magrath@newsignature.com>
2020-12-28 08:15:28 +0000

    merge with master

--- deploy_azure_policies.sh
+++
@@ -1,8 +0,0 @@
-
-
-# Create the Policy Definition (Subscription scope)
-az policy definition create --name audit-existing-linux-vm-ssh-with-password --display-name "Audit existing Linux VMs that use password for SSH authentication" --description "This policy audits if a password is being used to authentication to a Linux VM" --rules ./azure_policies/azurepolicies.rules.json --mode All
-
-# Create the Policy Assignment
-# Set the scope to a resource group; may also be a subscription or management group
-az policy assignment create --name 'audit-existing-linux-vm-ssh-with-password-assignment' --display-name "Audit existing Linux VMs that use password for SSH authentication Assignment" --scope /subscriptions/<subscriptionId>/resourceGroups/<resourceGroupName> --policy /subscriptions/<subscriptionId>/providers/Microsoft.Authorization/policyDefinitions/audit-existing-linux-vm-ssh-with-password
