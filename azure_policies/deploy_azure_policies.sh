3dca7347b2595955cc69158bd3fa61ae46d65b9c
jared.magrath <jared.magrath@newsignature.com>
2020-12-28 08:17:39 +0000

    clean up of old files and code

--- azure_policies/deploy_azure_policies.sh
+++ azure_policies/deploy_azure_policies.sh
@@ -1,19 +1,6 @@
 
 
 #!/bin/sh
-if [ $1 ]; then
-   echo subscription_id: $1
-else
-  echo subscription_id must be provided as first argument eg. ./deploy_azure_policies.sh SUBSCRIPTION_ID RESOURCE_GROUP_NAME 
-  read -p "Press [Enter] key to close"
-fi
-
-if [ $2 ]; then
-   echo resource_group_name: $2
-else
-  echo resource_group_name must be provided as second argument eg. ./deploy_azure_policies.sh SUBSCRIPTION_ID RESOURCE_GROUP_NAME 
-  read -p "Press [Enter] key to close"
-fi
 
 # Create the Policy Definition (Subscription scope)
 az policy definition create --name "tagging-policy" --display-name "Deny creation of resources without tags" --description "prevent any new resources from being created without tags"  --rules ./azurepolicies.json --mode All
